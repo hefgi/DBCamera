e1d1ce0398b45ba27455a7959f02e408d851e0fe
hefgi <fj@merchlar.com>
2014-03-19 20:36:05 -0400

    adding a custom navigation controller

---
+++ DBCamera/DBNavigationViewController.m
@@ -0,0 +1,46 @@
+//
+//  DBNavigationViewController.m
+//  DBCamera
+//
+//  Created by FrancoisJulien ALCARAZ on 3/19/2014.
+//  Copyright (c) 2014 PSSD - Daniele Bogo. All rights reserved.
+//
+
+#import "DBNavigationViewController.h"
+
+@interface DBNavigationViewController ()
+
+@end
+
+@implementation DBNavigationViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (NSArray *)dataArray {
+    
+    NSArray * array = [NSArray arrayWithObjects:[NSArray arrayWithObjects:[self image1], [self metaData1], nil],[NSArray arrayWithObjects:[self image2], [self metaData2], nil], nil];
+    
+    return array;
+    
+}
+
+@end
